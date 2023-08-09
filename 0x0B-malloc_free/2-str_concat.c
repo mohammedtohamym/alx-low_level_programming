8e7b768b301621f625a0ddc9456deb28e730167c
mohammedtohamy <mohammedtohamym@gmail.com>
2023-08-09 03:41:30 +0300

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,8 +3,8 @@
 
 /**
  * str_concat - Concatenates
- * @s1:  first 
- * @s2:  second 
+ * @s1:  first
+ * @s2:  second
  *
  * Return: The pointer to the array or NULL
  */
@@ -14,21 +14,17 @@ char *str_concat(char *s1, char *s2)
 	int x, y;
 	char *string;
 
-	for (x = 0; s1 != NULL && s1[x] != '\0';){
+	for (x = 0; s1 != NULL && s1[x] != '\0';)
 		x = x + 1;
-	}
-	for (y = 0; s2 != NULL && s2[y] != '\0';){
+	for (y = 0; s2 != NULL && s2[y] != '\0';)
 		y = y + 1;
-	}
 	string = malloc(x + y + 1);
 	if (string)
 	{
-		for (x = 0; s1 != NULL && s1[x] != '\0'; x = x + 1){
+		for (x = 0; s1 != NULL && s1[x] != '\0'; x = x + 1)
 			string[x] = s1[x];
-		}
-		for (y = 0; s2 != NULL && s2[y] != '\0'; y = y + 1){
+		for (y = 0; s2 != NULL && s2[y] != '\0'; y = y + 1)
 			string[x+y] = s2[y];
-		}
 		string[x+y] = '\0';
 		return (string);
 	}
