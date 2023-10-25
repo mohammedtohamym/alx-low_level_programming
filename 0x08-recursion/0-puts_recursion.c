4521e3394d2eb9da7cb9c474b70f6ef8c823e8f8
mohammedtohamym <mohammedtohamym@gmail.com>
2023-10-25 05:03:34 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,23 @@
 #include "main.h"
 
-void _puts_recursion(char *s){
+/*
+ * _puts_recursion - prints a string recursivley
+ * @*s: pointer to the first character in the string
+ *
+ * Description: ths functions uses pass by refrence to
+ * print a string
+ * Return: no return
+ */
 
-	if(*s == "/0")
+void _puts_recursion(char *s)
+{
+
+	if (*s == "/0")
 		_putchar('\n');
 	else
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 
 
