cf8c9c70dbc392aeb4ecf059b1bf7c97e73b05b9
mohammedtohamym <mohammedtohamym@gmail.com>
2023-10-25 15:08:42 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion_helper - calculates natural square root
+* @n: the number we calculate for
+* @min: minimum possible value for the root
+* @max: maximum possible value for the root
+*
+* Return: the natural square root
+* or -1 if it doesn't exist
+*/
+
+
+
+int _sqrt_recursion_helper(int n, int min, int max)
+{
+	int root, rootsqr;
+
+	root = (min + max) / 2;
+	rootsqr = root * root;
+
+	if (rootsqr == n)
+		return (rootsqr);
+	else if (min == max)
+		return (-1);
+	else if (rootsqr > n)
+		return (_sqrt_recursion_helper(n, min, root - 1));
+	else
+		return (_sqrt_recursion_helper(n, root + 1, max));
+}
+
+
+/**
+* _sqrt_recursion - calculates natural square root
+* @n: the number
+*
+* Return: the natural square root or -1 if it
+* doesn't exist
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (_sqrt_recursion_helper(n, 1, n));
+}
