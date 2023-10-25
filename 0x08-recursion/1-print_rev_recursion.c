3cc47294abbe62605b44a9d4126cb2b791408277
mohammedtohamym <mohammedtohamym@gmail.com>
2023-10-25 10:33:52 -0700

    reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,16 +2,14 @@
 
 /**
  *_print_rev_recursion - a function that prints a string in reverse
- * 
+ *
  * @s: a pointer to the first character in the string
  *
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	else
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
