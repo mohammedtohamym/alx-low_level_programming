a21531d1e3f70897ba06f8435fd1c88ae3655053
mohammedtohamym <mohammedtohamym@gmail.com>
2023-10-25 13:04:54 -0700

    powre

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _pow_recursion - raises a number to the power of another
+* @x: the base number
+* @y: the power
+*
+* Return: the result of the caluculation
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
