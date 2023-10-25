13eff29c143ba05a5402526645ec34253c0d362f
mohammedtohamym <mohammedtohamym@gmail.com>
2023-10-25 13:07:38 -0700

    powre

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,11 +6,14 @@
 * @y: the power
 *
 * Return: the result of the caluculation
+* or -1 if power is munis
 */
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
 		return (1);
 	else
 		return (x * _pow_recursion(x, y - 1));
