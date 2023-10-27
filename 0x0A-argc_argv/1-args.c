587eb5300adc02b8297fa48f982a0a9d21994f50
mohammedtohamym <mohammedtohamym@gmail.com>
2023-10-27 23:11:56 +0200

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print it's own program name
+ * @argc: the command line arguments cound
+ * @argv: the vector containing all the comman line arguemnts
+ *
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%s\n", argc);
+	return (0);
+}
