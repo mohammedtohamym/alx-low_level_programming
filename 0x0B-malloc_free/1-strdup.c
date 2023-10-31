1fb809ceeb917cdd6877d805f404d5200ef95ea2
mohammedtohamym <mohammedtohamym@gmail.com>
2023-10-31 15:31:43 -0700

    task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+* *_strdup - a function
+*
+* @str: input
+*
+* Return: NULL or s
+*
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, len = 0;
+	char *s;
+	if (str == NULL)
+		return (NULL);
+
+	/*calculate size of str*/
+	while (str[len] != '\0')
+		len++;
+	s = malloc((len + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+	}
+	return (s);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - a funnction
+ *
+ * @s1: input 1
+ * @s2: input 2
+ *
+ * Return: NULL or s 
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int k = 0, m = 0;
+	int i, j;
+	char *s;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[k] != '\0')
+		k++;
+	while (s2[m] != '\0')
+		m++;
+	s = malloc((k * sizeof(char)) + ((m + 1) * sizeof(char)));
+
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		s[i] = s2[j];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
